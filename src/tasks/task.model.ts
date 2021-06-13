481b02ef16f3d6c9a938613f3c85235c45ab5696
ukyoda <black12172001@hotmail.co.jp>
2021-06-14 08:36:00 +0900

    task-filter追加

--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -2,5 +2,7 @@ export interface Task {
   id: string;
   title: string;
   description: string;
-  status: 'OPEN' | 'IN_PROGRESS' | 'DONE';
+  status: TaskStatus;
 }
+
+export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
