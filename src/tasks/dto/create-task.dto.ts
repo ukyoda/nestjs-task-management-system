589a6d990e7a79290801aa1d1bd3f17eddc849cb
ukyoda <black12172001@hotmail.co.jp>
2021-06-14 09:02:36 +0900

    CreateTaskDtoにIsNotEmptyを追加

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,13 @@
+// <<Memo>>
 // APIのパラメータが新しく追加されたとき、
 // DTOを使わない場合はコントローラ、サービス、Etcすべて修正しないといけない
-// DTOを使っている場合は、このクラスにフィールドを1つ追加するだけで良い
+// DTOを使っている場合は、このクラスにフィールドを1つ追加するだけで解決できる
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
