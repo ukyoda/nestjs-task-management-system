c4437144c3e852672fc28ed6c2df68fca63994d2
ukyoda <black12172001@hotmail.co.jp>
2021-06-14 09:46:25 +0900

    set validation rule that max length is 140 characters for 'description'

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -2,12 +2,13 @@
 // APIのパラメータが新しく追加されたとき、
 // DTOを使わない場合はコントローラ、サービス、Etcすべて修正しないといけない
 // DTOを使っている場合は、このクラスにフィールドを1つ追加するだけで解決できる
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength } from 'class-validator';
 
 export class CreateTaskDto {
   @IsNotEmpty()
   title: string;
 
   @IsNotEmpty()
+  @MaxLength(140)
   description: string;
 }
