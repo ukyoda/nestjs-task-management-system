1e91f04f7c868c92798a0cf5482fbde911a541ef
ukyoda <black12172001@hotmail.co.jp>
2021-06-19 14:56:15 +0900

    認証モジュール。「とりあえず」ユーザを作るところまで実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dt';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersRepository)
+        private usersRepository: UsersRepository,
+    ) {}
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.usersRepository.createUser(authCredentialsDto);
+    }
+}
---
+++ src/auth/users.repository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dt';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+
+}
